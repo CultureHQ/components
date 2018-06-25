ef1582c93087cdced95c3792d8a89c388e4cb20e
Kevin Deisz <kevin.deisz@gmail.com>
2018-06-25 11:04:16 -0400

    Bump to 0.0.12

---
+++ webpack.prod.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  output: {
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [{ test: /\.js$/, use: "babel-loader", exclude: /node_modules/ }]
+  },
+  target: "node",
+  mode: "production"
+};
