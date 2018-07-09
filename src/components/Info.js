eb665cb27a39b255ac195a7f5405dcd2643d8b1a
Kevin Deisz <kevin.deisz@gmail.com>
2018-07-09 13:50:01 -0400

    Convert Info to SCSS

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,23 +1,10 @@
 import React from "react";
-import styled from "styled-components";
+import classnames from "classnames";
 
-import { colors } from "../common.json";
-
-const Container = styled.div`
-  background-color: ${colors.info.background};
-  color: ${colors.primaryFont};
-  margin-bottom: 20px;
-  padding: 15px;
-`;
-
-const Text = styled.p`
-  margin: 0;
-`;
-
-const Info = ({ children, ...props }) => (
-  <Container {...props}>
-    <Text>{children}</Text>
-  </Container>
+const Info = ({ children, className, ...props }) => (
+  <div className={classnames(className, "chq-inf")} {...props}>
+    <p>{children}</p>
+  </div>
 );
 
 export default Info;
