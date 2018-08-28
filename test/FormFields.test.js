c5b9ef316fd6682114d05bc99b2c4e229c0b0b4e
Kevin Deisz <kevin.deisz@gmail.com>
2018-08-28 13:07:35 -0400

    Tests for focus

--- test/FormFields.test.js
+++ test/FormFields.test.js
@@ -1,46 +1,51 @@
 import React from "react";
 import { mount } from "enzyme";
 
-import {
-  EmailField,
-  NumberField,
-  PasswordField,
-  StringField
-} from "../src";
+import * as FormFields from "../src/components/FormFields";
 
-[EmailField, NumberField, PasswordField, StringField].forEach(FormField => {
-  test("passes on className", () => {
-    const component = mount(<FormField name="name" className="form-field" />);
+const CASES = Object.keys(FormFields).map(fieldName => [FormFields[fieldName]]);
 
-    expect(component.find("label").hasClass("chq-ffd")).toBe(true);
-    expect(component.find("label").hasClass("form-field")).toBe(true);
+test.each(CASES)("passes on className", FormField => {
+  const component = mount(<FormField name="name" className="form-field" />);
+
+  expect(component.find("label").hasClass("chq-ffd")).toBe(true);
+  expect(component.find("label").hasClass("form-field")).toBe(true);
+});
+
+test.each(CASES)("calls up to callbacks if they are provided", FormField => {
+  const response = {
+    changeValue: null,
+    formChangeName: null,
+    formChangeValue: null
+  };
+
+  const component = mount(
+    <FormField
+      name="name"
+      onChange={changeValue => {
+        Object.assign(response, { changeValue });
+      }}
+      onFormChange={(formChangeName, formChangeValue) => {
+        Object.assign(response, { formChangeName, formChangeValue });
+      }}
+    />
+  );
+
+  component.find("input").simulate("change", { target: { value: "Kevin" } });
+
+  expect(response).toEqual({
+    changeValue: "Kevin",
+    formChangeName: "name",
+    formChangeValue: "Kevin"
   });
+});
 
-  test("calls up to callbacks if they are provided", () => {
-    const response = {
-      changeValue: null,
-      formChangeName: null,
-      formChangeValue: null
-    };
+test.each(CASES)("tracks focus in component state", FormField => {
+  const component = mount(<FormField name="name" required />);
 
-    const component = mount(
-      <FormField
-        name="name"
-        onChange={changeValue => {
-          Object.assign(response, { changeValue });
-        }}
-        onFormChange={(formChangeName, formChangeValue) => {
-          Object.assign(response, { formChangeName, formChangeValue });
-        }}
-      />
-    );
+  component.find("input").simulate("blur");
+  expect(component.text()).toEqual("Required");
 
-    component.find("input").simulate("change", { target: { value: "Kevin" } });
-
-    expect(response).toEqual({
-      changeValue: "Kevin",
-      formChangeName: "name",
-      formChangeValue: "Kevin"
-    });
-  });
+  component.find("input").simulate("focus");
+  expect(component.text()).toEqual("");
 });
