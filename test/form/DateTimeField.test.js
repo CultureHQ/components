3fefbb6017e69931a46a04b2263179ede4f2cbdb
Kevin Deisz <kevin.deisz@gmail.com>
2018-10-24 18:17:30 -0400

    Fully test out date time field

--- test/form/DateTimeField.test.js
+++ test/form/DateTimeField.test.js
@@ -1,28 +1,60 @@
 import React, { Component } from "react";
 import { mount } from "enzyme";
 
-import { DateTimeField } from "../../src";
-
-class DateTimeFieldContainer extends Component {
-  state = { value: new Date(2018, 0, 1, 0, 0, 0) };
-
-  handleChange = value => {
-    this.setState({ value });
-  };
-
-  render() {
-    const { value } = this.state;
-
-    return <DateTimeField value={value} onChange={this.handleChange} />;
-  }
-}
+import { DateTimeField, Form } from "../../src";
 
 test("functions as expected", () => {
-  const component = mount(<DateTimeFieldContainer />);
+  let received = null;
+  const onSubmit = values => {
+    received = values;
+    return Promise.resolve();
+  };
+
+  const component = mount(
+    <Form onSubmit={onSubmit} initialValues={{ datetime: new Date(2018, 0, 1, 0, 0, 0).toISOString() }}>
+      <DateTimeField name="datetime" />
+    </Form>
+  );
 
   component.find("button.chq-ffd--dt").simulate("click");
   component.find("button.chq-cal--day").at(15).simulate("click");
   component.find("button.chq-tsl--op").at(52).simulate("click");
+  component.instance().submit();
 
-  expect(component.state().value).toEqual("2018-01-15T13:00:00.000Z");
+  expect(received.datetime).toEqual("2018-01-15T13:00:00.000Z");
+});
+
+test("works without a value passed in", () => {
+  const component = mount(<DateTimeField />);
+
+  expect(component.find("button")).toHaveLength(1);
+});
+
+test("allows clicking on select before making a time selection", () => {
+  let received = null;
+  const onChange = value => {
+    received = value;
+  };
+
+  const component = mount(<DateTimeField onChange={onChange} />);
+
+  component.find("button.chq-ffd--dt").simulate("click");
+  component.find("button.chq-cal--day").at(15).simulate("click");
+  component.find("Button").simulate("click");
+
+  expect(received.endsWith("T12:00:00.000Z")).toBe(true);
+});
+
+test("works when clicking on a time before a date", () => {
+  let received = null;
+  const onChange = value => {
+    received = value;
+  };
+
+  const component = mount(<DateTimeField onChange={onChange} />);
+
+  component.find("button.chq-ffd--dt").simulate("click");
+  component.find("button.chq-tsl--op").at(52).simulate("click");
+
+  expect(received.endsWith("T13:00:00.000Z")).toBe(true);
 });
