f92cdd413b33fdb0dffb936d81ca818a8c9c1270
Kevin Deisz <kevin.deisz@gmail.com>
2018-10-24 17:20:41 -0400

    Better display names in dev tools for plain button

--- src/components/buttons/PlainButton.js
+++ src/components/buttons/PlainButton.js
@@ -3,9 +3,11 @@ import React from "react";
 import classnames from "../../classnames";
 
 const PlainButton = React.forwardRef(({ children, className, ...props }, ref) => (
-  <button ref={ref} {...props} type="button" className={classnames("chq-pbn", className)}>
+  <button {...props} ref={ref} type="button" className={classnames("chq-pbn", className)}>
     {children}
   </button>
 ));
+
+PlainButton.displayName = "PlainButton";
 
 export default PlainButton;
