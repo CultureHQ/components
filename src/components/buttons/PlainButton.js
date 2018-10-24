d40b5d128cc29f370913bcf7aab3730ec3493731
Kevin Deisz <kevin.deisz@gmail.com>
2018-10-24 17:16:42 -0400

    Automatically scroll to active time

--- src/components/buttons/PlainButton.js
+++ src/components/buttons/PlainButton.js
@@ -2,10 +2,10 @@ import React from "react";
 
 import classnames from "../../classnames";
 
-const Button = ({ children, className, ...props }) => (
-  <button {...props} type="button" className={classnames("chq-pbn", className)}>
+const PlainButton = React.forwardRef(({ children, className, ...props }, ref) => (
+  <button ref={ref} {...props} type="button" className={classnames("chq-pbn", className)}>
     {children}
   </button>
-);
+));
 
-export default Button;
+export default PlainButton;
