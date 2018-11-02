a77eca87721f90382df92fb1d77c8bc43ffa1640
Kevin Deisz <kevin.deisz@gmail.com>
2018-11-02 16:33:04 -0400

    Split up styles better

--- src/components/form/ImageFieldPreview.js
+++ src/components/form/ImageFieldPreview.js
@@ -1,51 +1,6 @@
 import React, { Component } from "react";
 
-import getRotation from "../../utils/get-rotation";
-
-const ROTATIONS = [
-  {},
-  { transform: "rotateY(180deg)" },
-  { transform: "rotate(180deg)" },
-  { transform: "rotate(180deg) rotateY(180deg)" },
-  { transform: "rotate(270deg) rotateY(180deg)", transformOrigin: "top left" },
-  { transform: "translateY(-100%) rotate(90deg)", transformOrigin: "bottom left" },
-  {
-    transform: "translateY(-100%) translateX(-100%) rotate(90deg) rotateY(180deg)",
-    transformOrigin: "bottom right"
-  },
-  { transform: "translateX(-100%) rotate(270deg)", transformOrigin: "top right" }
-];
-
-const getStylesForRotation = (image, rotation, maxHeight) => {
-  const scaledWidth = image.width / image.height * maxHeight;
-  const scaledHeight = image.height / image.width * maxHeight;
-
-  const halfWidth = (rotation >= 5 ? scaledHeight : scaledWidth) / 2;
-
-  return {
-    ...ROTATIONS[rotation - 1],
-    left: `calc(50% - ${halfWidth}px)`,
-    height: rotation >= 5 ? scaledHeight : maxHeight
-  };
-};
-
-const setupImage = (preview, maxHeight) => {
-  const image = new Image();
-  image.src = typeof preview === "string" ? preview : URL.createObjectURL(preview);
-
-  const promises = [
-    getRotation(preview),
-    new Promise((resolve, reject) => {
-      image.onload = resolve;
-      image.onerror = reject;
-    })
-  ];
-
-  return Promise.all(promises).then(([rotation]) => ({
-    src: image.src,
-    styles: getStylesForRotation(image, rotation, maxHeight)
-  }));
-};
+import readImage from "../../utils/read-image";
 
 class ImageFieldPreview extends Component {
   imageRef = React.createRef();
@@ -84,7 +39,7 @@ class ImageFieldPreview extends Component {
     const imageTag = this.imageRef.current;
     const maxHeight = imageTag ? imageTag.parentNode.clientHeight : 198;
 
-    setupImage(preview, maxHeight).then(({ src, styles }) => {
+    readImage(preview, maxHeight).then(({ src, styles }) => {
       if (this.componentIsMounted) {
         this.setState({ src, styles });
       }
---
+++ src/utils/read-image.js
@@ -0,0 +1,48 @@
+import getRotation from "./get-rotation";
+
+const ROTATIONS = [
+  {},
+  { transform: "rotateY(180deg)" },
+  { transform: "rotate(180deg)" },
+  { transform: "rotate(180deg) rotateY(180deg)" },
+  { transform: "rotate(270deg) rotateY(180deg)", transformOrigin: "top left" },
+  { transform: "translateY(-100%) rotate(90deg)", transformOrigin: "bottom left" },
+  {
+    transform: "translateY(-100%) translateX(-100%) rotate(90deg) rotateY(180deg)",
+    transformOrigin: "bottom right"
+  },
+  { transform: "translateX(-100%) rotate(270deg)", transformOrigin: "top right" }
+];
+
+const getRotationStyles = (image, rotation, maxHeight) => {
+  const scaledWidth = image.width / image.height * maxHeight;
+  const scaledHeight = image.height / image.width * maxHeight;
+
+  const halfWidth = (rotation >= 5 ? scaledHeight : scaledWidth) / 2;
+
+  return {
+    ...ROTATIONS[rotation - 1],
+    left: `calc(50% - ${halfWidth}px)`,
+    height: rotation >= 5 ? scaledHeight : maxHeight
+  };
+};
+
+const readImage = (preview, maxHeight) => {
+  const image = new Image();
+  image.src = typeof preview === "string" ? preview : URL.createObjectURL(preview);
+
+  const promises = [
+    getRotation(preview),
+    new Promise((resolve, reject) => {
+      image.onload = resolve;
+      image.onerror = reject;
+    })
+  ];
+
+  return Promise.all(promises).then(([rotation]) => ({
+    src: image.src,
+    styles: getRotationStyles(image, rotation, maxHeight)
+  }));
+};
+
+export default readImage;
