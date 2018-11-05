fa25d97755a23392ee5228122da01276f2548b7e
Kevin Deisz <kevin.deisz@gmail.com>
2018-11-05 09:29:37 -0500

    Test out image field preview

--- test/form/ImageFieldPreview.test.js
+++ test/form/ImageFieldPreview.test.js
@@ -19,3 +19,25 @@ test("reads image and loads it", async () => {
   expect(component.find("img")).toHaveLength(1);
   expect(component.find("img").prop("style")).toHaveProperty("height", "200px");
 });
+
+test("does not attempt to set state when unmounted while waiting", async () => {
+  readImage.default = jest.fn(() => new Promise(resolve => (
+    setTimeout(() => resolve({ src: "culturehq.png", styles: { height: "200px" } }), 200)
+  )));
+
+  const component = mount(<ImageFieldPreview preview="culture.png" />);
+  expect(component.find("img")).toHaveLength(0);
+
+  const promise = component.instance().enqueueLoad();
+  component.unmount();
+  await promise;
+});
+
+test("references parent height if already loaded", () => {
+  const component = mount(<div><ImageFieldPreview preview="culturehq.png" /></div>);
+
+  component.find("ImageFieldPreview").instance().setState({ src: "culturehq.png" });
+  component.update();
+
+  component.find("ImageFieldPreview").instance().enqueueLoad();
+});
