eba2ba73e89e6529ff5a1edeecfb66dac0867f90
Kevin Deisz <kevin.deisz@gmail.com>
2018-11-13 11:56:16 -0500

    More tests for SelectField

--- src/components/form/__tests__/SelectField.test.js
+++ src/components/form/__tests__/SelectField.test.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { mount } from "enzyme";
 
 import SelectField from "../SelectField";
@@ -8,6 +8,39 @@ const OPTIONS = [
   { label: "Hermione", value: "hermione" },
   { label: "Ron", value: "ron" }
 ];
+
+const Container = ({ creatable = false, multiple = false }) => {
+  const [value, setValue] = useState("");
+
+  return (
+    <SelectField
+      name="select"
+      options={OPTIONS}
+      value={value}
+      onChange={setValue}
+      creatable={creatable}
+      multiple={multiple}
+    >
+      Select
+    </SelectField>
+  );
+};
+
+const mountWithUtils = jsx => {
+  const component = mount(jsx);
+
+  return Object.assign(component, {
+    getMultiValue() {
+      return component.find("input[type='hidden']").map(node => node.props().value);
+    },
+    matchSingleText(value) {
+      component.find(".chq-ffd--ctrl").simulate("change", { target: { value } });
+    },
+    matchMultiText(value) {
+      component.find(".chq-ffd--sl--match").simulate("change", { target: { value } });
+    }
+  });
+};
 
 test("has no violations", async () => {
   const jsx = (
@@ -59,26 +92,79 @@ test("requests focus when autoFocus is given", () => {
   expect(document.activeElement.className).toEqual("chq-ffd--ctrl");
 });
 
-test.only("working with a single non-creatable field", () => {
-  const component = mount(<SelectField name="select" options={OPTIONS}>Select</SelectField>);
+test("working with a single non-creatable field", () => {
+  const component = mountWithUtils(<Container />);
 
-  component.matchText = value => {
-    component.find(".chq-ffd--ctrl").simulate("change", { target: { value } });
-  };
-
-  component.matchText("H");
+  component.matchSingleText("H");
   expect(component.find("SelectFieldOption")).toHaveLength(2);
 
-  component.matchText("He");
+  component.matchSingleText("He");
   expect(component.find("SelectFieldOption")).toHaveLength(1);
 
-  component.matchText("Hel");
+  component.matchSingleText("Hel");
   expect(component.find("SelectFieldOption")).toHaveLength(0);
   expect(component.find("p")).toHaveLength(1);
 
-  component.matchText("");
+  component.matchSingleText("");
   expect(component.find("SelectFieldOption")).toHaveLength(3);
 
   component.find("SelectFieldOption").at(0).simulate("click");
+  expect(component.find("#select").props().value).toEqual(OPTIONS[0].value);
   expect(component.find(".chq-ffd--ctrl").props().value).toEqual(OPTIONS[0].label);
+
+  const twice = callback => [0, 1].forEach(callback);
+
+  twice(() => {
+    component.find(".chq-ffd--ctrl").simulate("keydown", { key: "Enter" });
+    expect(component.find("DoorEffect").props().open).toBe(true);
+  });
+
+  twice(() => {
+    component.find(".chq-ffd--ctrl").simulate("keydown", { key: "Escape" });
+    expect(component.find("DoorEffect").props().open).toBe(false);
+  });
+
+  component.find(".chq-ffd--ctrl").simulate("keydown", { key: "Backspace" });
+  expect(component.find("DoorEffect").props().open).toBe(false);
+});
+
+test("working with a single creatable field", () => {
+  const component = mountWithUtils(<Container creatable />);
+
+  component.matchSingleText("Hello");
+  expect(component.find("SelectFieldOption")).toHaveLength(1);
+  expect(component.find("p")).toHaveLength(0);
+
+  component.find("SelectFieldOption").simulate("click");
+  expect(component.find("#select").props().value).toEqual("Hello");
+  expect(component.find(".chq-ffd--ctrl").props().value).toEqual("Hello");
+});
+
+test("working with a multiple non-creatable field", () => {
+  const component = mountWithUtils(<Container multiple />);
+
+  component.matchMultiText("H");
+  expect(component.find("SelectFieldOption")).toHaveLength(2);
+
+  component.find("SelectFieldOption").at(0).simulate("click");
+  expect(component.find("Badge")).toHaveLength(1);
+  expect(component.getMultiValue()).toEqual([OPTIONS[0].value]);
+
+  component.find("SelectFieldOption").at(1).simulate("click");
+  expect(component.find("Badge")).toHaveLength(2);
+  expect(component.getMultiValue()).toEqual([OPTIONS[0].value, OPTIONS[1].value]);
+
+  component.find("Badge").at(0).simulate("click");
+  expect(component.getMultiValue()).toEqual([OPTIONS[1].value]);
+});
+
+test("working with a multiple creatable field", () => {
+  const component = mountWithUtils(<Container creatable multiple />);
+
+  component.matchMultiText("Hello");
+  expect(component.find("SelectFieldOption")).toHaveLength(1);
+  expect(component.find("p")).toHaveLength(0);
+
+  component.find("SelectFieldOption").simulate("click");
+  expect(component.getMultiValue()).toEqual(["Hello"]);
 });
