2901dddc3ef271a0f15f09c55fffbc1f731703b9
Kevin Deisz <kevin.deisz@gmail.com>
2018-12-23 11:25:20 -0500

    Fix up lint

--- docs/Components.js
+++ docs/Components.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import /* codegen */ "./Main";
+/* eslint-disable react/jsx-no-undef */
 
 import { Modal as ModalSrc, Nav as NavSrc } from "../src/components";
 import "../src/styles/app.scss";
