b702ae43992ee10e125fbd9646d2fb4c4a3200b0
Kevin Deisz <kevin.deisz@gmail.com>
2019-01-28 16:28:54 -0500

    RadioField

---
+++ src/components/form/RadioField.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+
+import classnames from "../../classnames";
+import FormError from "./FormError";
+import { withForm } from "./Form";
+
+class RadioField extends Component {
+  static defaultProps = {
+    onChange: () => {},
+    onFormChange: () => {},
+    options: [],
+    values: {}
+  };
+
+  state = { touched: false };
+
+  handleBlur = () => {
+    this.setState({ touched: true });
+  };
+
+  handleChange = ({ target: { value } }) => {
+    const { name, onChange, onFormChange } = this.props;
+
+    onChange(value);
+    onFormChange(name, value);
+  };
+
+  render() {
+    const {
+      children, className, errors, name, onError, onFormChange, options,
+      required, submitted, submitting, validator, value, values, ...props
+    } = this.props;
+
+    const { touched } = this.state;
+
+    const normal = value || values[name];
+
+    return (
+      <fieldset className={classnames("chq-ffd", className)} {...props}>
+        <legend className="chq-ffd--lb">{children}</legend>
+        {options.map((option, index) => (
+          <label key={option.value} className="chq-ffd--radio" htmlFor={`${name}${index + 1}`}>
+            <input
+              type="radio"
+              id={`${name}${index + 1}`}
+              name={name}
+              value={option.value}
+              checked={option.value.toString() === normal}
+              onBlur={this.handleBlur}
+              onChange={this.handleChange}
+            />
+            <em className="chq-ffd--radio-check" />
+            {" "}
+            {option.label}
+          </label>
+        ))}
+        <FormError
+          name={name}
+          onError={onError}
+          required={required}
+          submitted={submitted}
+          touched={touched}
+          validator={validator}
+          value={normal}
+        />
+      </fieldset>
+    );
+  }
+}
+
+export default withForm(RadioField);
---
+++ src/components/form/__tests__/RadioField.test.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { mount } from "enzyme";
+
+import RadioField from "../RadioField";
+import Form from "../Form";
+
+const testOptions = [
+  { value: "gryffindor", label: "Gryffindor" },
+  { value: "hufflepuff", label: "Hufflepuff" },
+  { value: "ravenclaw", label: "Ravenclaw" },
+  { value: "slytherin", label: "Slytherin" }
+];
+
+test("has no violations", async () => {
+  await expect(
+    <RadioField name="radio" options={testOptions}>
+      Radio
+    </RadioField>
+  ).toHaveNoViolations();
+});
+
+test("passes on className", () => {
+  const component = mount(<RadioField name="radio" className="radio" />);
+
+  expect(component.find(".chq-ffd").hasClass("radio")).toBe(true);
+});
+
+test("calls up to callbacks if they are provided", () => {
+  const onChange = jest.fn();
+  const component = mount(
+    <Form>
+      <RadioField name="radio" options={testOptions} onChange={onChange} />
+    </Form>
+  );
+
+  component.find("label input").at(0).simulate("change", {
+    target: { value: testOptions[0].value }
+  });
+
+  expect(onChange).toHaveBeenCalledWith(testOptions[0].value);
+});
+
+test("tracks touch status in component state", () => {
+  const component = mount(
+    <RadioField required name="radio" options={testOptions} />
+  );
+
+  component.find("input").at(0).simulate("change", { target: { value: null } });
+  component.find("input").at(0).simulate("blur");
+
+  expect(component.find("FormError").text()).toEqual("Required");
+});
+
+test("displays errors if submitted", () => {
+  const component = mount(<Form><RadioField name="radio" options={testOptions} required /></Form>);
+  expect(component.find("FormError").text()).toEqual(null);
+
+  component.setState({ submitted: true });
+  expect(component.find("FormError").text()).toEqual("Required");
+});
