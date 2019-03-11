2c645a86a2aceb95628eee24c945d64a17403ee9
Kevin Deisz <kevin.deisz@gmail.com>
2019-03-11 11:41:44 -0400

    Bump dependencies

--- src/components/__tests__/__mocks__/image.js
+++ src/components/__tests__/__mocks__/image.js
@@ -2,8 +2,6 @@ import fs from "fs";
 import path from "path";
 
 const FILEPATH = path.join(__dirname, "image.jpg");
-
-const type = path.extname(FILEPATH).slice(1);
 const base64 = fs.readFileSync(FILEPATH).toString("base64");
 
-export default `data:image/${type};base64,${base64}`;
+export default `data:image/jpeg;base64,${base64}`;
