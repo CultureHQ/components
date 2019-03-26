5ce96c27bc7a1fac374d5006524750d798b78823
Kevin Deisz <kevin.deisz@gmail.com>
2019-03-26 15:25:30 -0400

    Add hooks into button props

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,10 +1,33 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { withKnobs, boolean, optionsKnob, text } from "@storybook/addon-knobs";
 
 import { Button } from "../src/components";
+import icons from "../src/icons.json";
+
+const iconOpts = Object.keys(icons).reduce(
+  (accum, icon) => ({ ...accum, [icon]: icon }), { none: null }
+);
 
 storiesOf("Button", module)
-  .add("default", () => <Button>Default</Button>)
+  .addDecorator(withKnobs)
+  .add("default", () => {
+    const children = text("children", "Default");
+    const props = {
+      danger: boolean("danger", false),
+      disabled: boolean("disabled", false),
+      fillParent: boolean("fillParent", false),
+      icon: optionsKnob("icon", iconOpts, null, { display: "select" }),
+      inverted: boolean("inverted", false),
+      loading: boolean("loading", false),
+      onClick: action("onClick"),
+      primary: boolean("primary", false),
+      small: boolean("small", false)
+    };
+
+    return <Button {...props}>{children}</Button>;
+  })
   .add("loading", () => <Button loading>Loading</Button>)
   .add("disabled", () => <Button disabled>Disabled</Button>)
   .add("danger", () => <Button danger>Danger</Button>)
@@ -15,3 +38,4 @@ storiesOf("Button", module)
   .add("icon + primary", () => <Button icon="clipboard" primary>Primary Icon</Button>)
   .add("icon + small", () => <Button icon="clipboard" small>Small Icon</Button>)
   .add("icon + inverted", () => <Button icon="clipboard" inverted>Inverted Icon</Button>);
+
