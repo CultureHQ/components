92eda36afdcaf16716b96f750c247bbb92b007c7
Kevin Deisz <kevin.deisz@gmail.com>
2019-03-26 15:45:50 -0400

    Add stories for checkmark

---
+++ stories/Checkmark.stories.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { withKnobs, boolean, text } from "@storybook/addon-knobs";
+
+import { Checkmark } from "../src/components";
+
+storiesOf("Checkmark", module)
+  .addDecorator(withKnobs)
+  .add("default", () => {
+    const children = text("children", null);
+    const props = {
+      checked: boolean("checked", false),
+      onClick: action("onClick")
+    };
+
+    return <Checkmark {...props}>{children}</Checkmark>
+  })
+  .add("checked", () => <Checkmark checked />);
