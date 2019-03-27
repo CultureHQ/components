a17835fe909517d30c4fc8e3f1041a94fd59a884
Kevin Deisz <kevin.deisz@gmail.com>
2019-03-27 09:26:26 -0400

    Stories for Checklist

---
+++ stories/Checklist.stories.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withKnobs, boolean, text } from "@storybook/addon-knobs";
+
+import { Checklist } from "../src/components";
+
+storiesOf("Checklist", module)
+  .addDecorator(withKnobs)
+  .add("default", () => {
+    const checked = {
+      first: boolean("first", true),
+      second: boolean("second", false)
+    };
+
+    return (
+      <Checklist>
+        <Checklist.Item checked={checked.first}>First</Checklist.Item>
+        <Checklist.Item checked={checked.second}>Second</Checklist.Item>
+      </Checklist>
+    );
+  });
