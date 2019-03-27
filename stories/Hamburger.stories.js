a865e075786781f57c09fcfc7b1eb6d23a3137bc
Kevin Deisz <kevin.deisz@gmail.com>
2019-03-27 09:23:03 -0400

    Add stories for remaining buttons

---
+++ stories/Hamburger.stories.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { withKnobs, boolean } from "@storybook/addon-knobs";
+
+import { Hamburger } from "../src/components";
+
+const Container = props => (
+  <div style={{ backgroundColor: "#2c3e4f", display: "inline-block" }}>
+    <Hamburger {...props} />
+  </div>
+);
+
+storiesOf("Hamburger", module)
+  .addDecorator(withKnobs)
+  .add("default", () => {
+    const props = {
+      onToggle: action("onToggle"),
+      open: boolean("open", false)
+    };
+
+    return <Container {...props} />;
+  })
+  .add("open", () => <Container open />);
