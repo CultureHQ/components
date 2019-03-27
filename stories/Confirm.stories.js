6161cef8578f3dd2275891703b67d4f8e350946b
Kevin Deisz <kevin.deisz@gmail.com>
2019-03-27 09:48:45 -0400

    Stories for Confirm

---
+++ stories/Confirm.stories.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { withKnobs, boolean, text } from "@storybook/addon-knobs";
+
+import { Button, Confirm } from "../src/components";
+
+storiesOf("Confirm", module)
+  .addDecorator(withKnobs)
+  .add("default", () => {
+    const trigger = text("trigger", "Create")
+    const children = text(
+      "children",
+      "Are you sure you would like to create this resource?"
+    );
+
+    const props = {
+      accept: text("accept", "Yes"),
+      danger: boolean("danger", false),
+      onAccept: action("onAccept")
+    };
+
+    return (
+      <Confirm
+        {...props}
+        trigger={onTrigger => <Button onClick={onTrigger}>{trigger}</Button>}
+      >
+        {children}
+      </Confirm>
+    );
+  })
+  .add("danger", () => (
+    <Confirm
+      danger
+      trigger={onTrigger => <Button danger onClick={onTrigger}>Delete</Button>}
+    >
+      Are you sure you would like to delete this resource?
+    </Confirm>
+  ));
