d095b582300d62a0588ec33cc1747c91837563dc
Kevin Deisz <kevin.deisz@gmail.com>
2019-03-27 09:32:57 -0400

    Stories for Circles

---
+++ stories/Circles.stories.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import { Circles } from "../src/components";
+
+storiesOf("Circles", module).add("default", () => <Circles />);
