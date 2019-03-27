209333ce3ccfd9ea6c8ccf14d5d7b933b0ef7443
Kevin Deisz <kevin.deisz@gmail.com>
2019-03-27 10:21:21 -0400

    Nav stories

---
+++ stories/Nav.stories.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import { Nav } from "../src/components";
+
+const Container = () => (
+  <>
+    <Nav>
+      <span style={{ fontSize: "1.5em", lineHeight: "1.75em" }}>
+        @culturehq/components
+      </span>
+    </Nav>
+    <div style={{ height: "1000px" }} />
+  </>
+);
+
+storiesOf("Nav", module).add("default", () => <Container />);
