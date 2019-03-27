89cf39c5b7abd1667abfea99953f8aee17fdc6fe
Kevin Deisz <kevin.deisz@gmail.com>
2019-03-27 10:07:31 -0400

    Add stories for Thumbnail

---
+++ stories/Thumbnail.stories.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withKnobs, boolean, optionsKnob, text } from "@storybook/addon-knobs";
+
+import { Thumbnail } from "../src/components";
+import image from "./culture.png";
+
+const sizeOptions = {
+  small: "small",
+  medium: "medium",
+  large: "large"
+};
+
+storiesOf("Thumbnail", module)
+  .addDecorator(withKnobs)
+  .add("default", () => {
+    const props = {
+      image,
+      size: optionsKnob("size", sizeOptions, "small", {
+        display: "inline-radio"
+      }),
+      square: boolean("square", false),
+      title: text("title", null)
+    };
+
+    return <Thumbnail {...props} />;
+  });
