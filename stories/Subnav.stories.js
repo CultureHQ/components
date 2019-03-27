fe869a8724d4c503ec00094b58b1593afbf8ebde
Kevin Deisz <kevin.deisz@gmail.com>
2019-03-27 10:12:33 -0400

    Subnav stories

---
+++ stories/Subnav.stories.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { withKnobs, number } from "@storybook/addon-knobs";
+
+import { Subnav } from "../src/components";
+
+storiesOf("Subnav", module)
+  .addDecorator(withKnobs)
+  .add("default", () => {
+    const props = {
+      activeIndex: number("activeIndex", 0),
+      onChange: action("onChange")
+    };
+
+    return (
+      <Subnav {...props}>
+        <Subnav.Item>One</Subnav.Item>
+        <Subnav.Item>Two</Subnav.Item>
+        <Subnav.Item>Three</Subnav.Item>
+      </Subnav>
+    );
+  });
