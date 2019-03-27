abf802044af997fea3bd596d5d8306775db547c7
Kevin Deisz <kevin.deisz@gmail.com>
2019-03-27 09:59:04 -0400

    Spinner stories

---
+++ stories/Spinner.stories.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import { Spinner } from "../src/components";
+
+storiesOf("Spinner", module).add("default", () => <Spinner />);
