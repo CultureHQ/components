9da1c3c52960eb1b732614d6cfe728526cd3d4d1
Kevin Deisz <kevin.deisz@gmail.com>
2019-03-27 14:32:43 -0400

    Add the a11y addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,13 @@
 import { addDecorator, addParameters, configure } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
+import { withA11y } from "@storybook/addon-a11y";
 
 import theme from "./theme";
 import "../src/styles/app.scss";
 import "./styles.css";
 
 addDecorator(withKnobs);
+addDecorator(withA11y);
 addParameters({ options: { theme } });
 
 const req = require.context("../stories", true, /\.stories\.js$/);
