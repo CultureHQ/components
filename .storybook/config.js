80bfccfa0217e852ab4c1405c124692dd653e734
Kevin Deisz <kevin.deisz@gmail.com>
2019-03-28 09:14:10 -0400

    Stories for Modal

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,13 +2,23 @@ import { addDecorator, addParameters, configure } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
 import { withA11y } from "@storybook/addon-a11y";
 
-import theme from "./theme";
+import { ModalDialog } from "../src/components";
 import "../src/styles/app.scss";
+
+import theme from "./theme";
 import "./styles.css";
+
+ModalDialog.setAppElement(document.body);
 
 addDecorator(withKnobs);
 addDecorator(withA11y);
-addParameters({ options: { theme } });
+
+addParameters({
+  options: {
+    panelPosition: "bottom",
+    theme
+  }
+});
 
 const req = require.context("../stories", true, /\.stories\.js$/);
 configure(() => req.keys().forEach(req), module);
--- stories/modals/Confirm.stories.js
+++ stories/modals/Confirm.stories.js
@@ -3,7 +3,7 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { boolean, text } from "@storybook/addon-knobs";
 
-import { Button, Confirm } from "../src/components";
+import { Button, Confirm } from "../../src/components";
 
 storiesOf("Modals/Confirm", module)
   .add("default", () => {
