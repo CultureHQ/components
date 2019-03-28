e2f0a206bfca5a8f3574ee04de5d306da272cd0d
Kevin Deisz <kevin.deisz@gmail.com>
2019-03-28 09:28:40 -0400

    Add BooleanField stories

---
+++ stories/form/BooleanField.stories.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { optionsKnob, text } from "@storybook/addon-knobs";
+
+import { BooleanField, Form } from "../../src/components";
+import iconOptions from "../utils/iconOptions";
+
+const valueOptions = {
+  null: "null",
+  true: "true",
+  false: "false"
+};
+
+storiesOf("Form/BooleanField", module)
+  .add("default", () => {
+    const children = text("children", "Boolean");
+    const value = optionsKnob("value", valueOptions, null, {
+      display: "inline-radio"
+    });
+
+    const props = {
+      onChange: action("onChange"),
+      name: text("name", "boolean"),
+      value: { null: null, true: true, false: false }[value]
+    };
+
+    return (
+      <Form>
+        <BooleanField {...props}>{children}</BooleanField>
+      </Form>
+    );
+  });
