3d8bcc382a38be862fb9d14a10ebf766207f0f0a
Kevin Deisz <kevin.deisz@gmail.com>
2019-04-01 09:55:33 -0400

    Bump deps and strip old docs

--- docs/main.46a4243f9cff2474e037.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{459:function(n,o,p){p(460),p(575),n.exports=p(1170)},575:function(n,o,p){"use strict";p.r(o);p(576),p(928),p(1131),p(1159)}},[[459,1,2]]]);
