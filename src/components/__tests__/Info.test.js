a7ae37610d4613578b430eacd4a0234b05ffcdb8
Kevin Deisz <kevin.deisz@gmail.com>
2019-06-18 16:28:27 -0400

    Switch over Info, Success, and Warning components to testing-library tests

--- src/components/__tests__/Info.test.js
+++ src/components/__tests__/Info.test.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { render } from "@testing-library/react";
 
 import Info from "../Info";
 
-test("has no violations", async () => {
-  await expect(<Info>This is an info.</Info>).toHaveNoViolations();
-});
-
 test("renders without crashing", () => {
   const message = "This is an info.";
-  const component = shallow(<Info>{message}</Info>);
+  const { queryByText } = render(<Info>{message}</Info>);
 
-  expect(component.html()).toContain(message);
+  expect(queryByText(message)).toBeTruthy();
 });
 
-test("passes on className", () => {
-  const component = shallow(<Info className="info" />);
+test("has no violations", () => (
+  expect(<Info>This is an info</Info>).toHaveNoViolations()
+));
 
-  expect(component.hasClass("info")).toBe(true);
+test("passes on className", () => {
+  const { container } = render(<Info className="info" />);
+
+  expect(container.querySelector(".info")).toBeTruthy();
 });
