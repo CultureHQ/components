ad00522302a59224234680ec89ca5c79a82058cd
Kevin Deisz <kevin.deisz@gmail.com>
2019-06-24 14:58:32 -0400

    Switch over Grid tests to RTL

--- src/components/__tests__/Grid.test.js
+++ src/components/__tests__/Grid.test.js
@@ -1,23 +1,38 @@
 import React from "react";
-import { mount } from "enzyme";
+import { render } from "@testing-library/react";
 
 import Grid from "../Grid";
 
-const makeHasClass = component => (size, cols) => (
-  component.find(".chq-grid--item").hasClass(`chq-grid--${size}-${cols}`)
-);
+test("has no violations", () => {
+  const jsx = (
+    <Grid>
+      <Grid.Item xs={3}>
+        Left panel
+      </Grid.Item>
+      <Grid.Item xs={6}>
+        Center content
+      </Grid.Item>
+      <Grid.Item xs={3}>
+        Right panel
+      </Grid.Item>
+    </Grid>
+  );
+
+  return expect(jsx).toHaveNoViolations();
+});
 
 test("creates main container", () => {
-  const component = mount(<Grid className="grid" />);
+  const { container } = render(<Grid className="grid" />);
 
-  expect(component.find(".chq-grid").hasClass("grid")).toBe(true);
+  expect(container.querySelector(".grid")).toBeTruthy();
 });
 
 test("applies classes", () => {
   const sizes = { xs: 12, sm: 6, md: 4, lg: 3, xl: 2 };
-  const hasClass = makeHasClass(mount(<Grid><Grid.Item {...sizes} /></Grid>));
+  const { container } = render(<Grid><Grid.Item {...sizes} /></Grid>);
 
   Object.keys(sizes).forEach(size => {
-    expect(hasClass(size, sizes[size])).toBe(true);
+    const className = `.chq-grid--item.chq-grid--${size}-${sizes[size]}`;
+    expect(container.querySelector(className)).toBeTruthy();
   });
 });
