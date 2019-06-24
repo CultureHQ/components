f74ca493626f80d910955feca819479580e0e08f
Kevin Deisz <kevin.deisz@gmail.com>
2019-06-24 14:55:01 -0400

    Switch over FeedItem tests

--- src/components/__tests__/FeedItem.test.js
+++ src/components/__tests__/FeedItem.test.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { render } from "@testing-library/react";
 
 import FeedItem from "../FeedItem";
 
-test("has no violations", async () => {
+test("has no violations", () => {
   const component = (
     <FeedItem>
       <FeedItem.Body>This is a feed item body.</FeedItem.Body>
@@ -11,26 +11,26 @@ test("has no violations", async () => {
     </FeedItem>
   );
 
-  await expect(component).toHaveNoViolations();
+  return expect(component).toHaveNoViolations();
 });
 
 test("renders without crashing", () => {
   const bodyMessage = "This is a feed item body.";
   const footerMessage = "This is a feed item footer.";
 
-  const component = shallow((
+  const { queryByText } = render(
     <FeedItem>
       <FeedItem.Body>{bodyMessage}</FeedItem.Body>
       <FeedItem.Footer>{footerMessage}</FeedItem.Footer>
     </FeedItem>
-  ));
+  );
 
-  expect(component.html()).toContain(bodyMessage);
-  expect(component.html()).toContain(footerMessage);
+  expect(queryByText(bodyMessage)).toBeTruthy();
+  expect(queryByText(footerMessage)).toBeTruthy();
 });
 
 test("passes on className", () => {
-  const component = shallow(<FeedItem className="feed-item" />);
+  const { container} = render(<FeedItem className="feed-item" />);
 
-  expect(component.hasClass("feed-item")).toBe(true);
+  expect(container.querySelector(".feed-item")).toBeTruthy();
 });
