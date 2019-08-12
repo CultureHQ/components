15e4ea2220b0aefdd1ff5b7b5e11520494742772
Kevin Deisz <kevin.deisz@gmail.com>
2019-08-12 15:36:40 -0400

    Switch ModalDialog over to RTL

--- src/components/modals/__tests__/ModalDialog.test.js
+++ src/components/modals/__tests__/ModalDialog.test.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { mount } from "enzyme";
-import ReactModal from "react-modal";
+import { render } from "@testing-library/react";
 
 import ModalDialog from "../ModalDialog";
 
 test("passes on class name", () => {
-  const component = mount(<ModalDialog className="modal-dialog" />);
+  render(<ModalDialog className="modal-dialog" />);
 
-  expect(component.find(ReactModal).hasClass("modal-dialog")).toBe(true);
-  expect(component.find(ReactModal).hasClass("chq-mdl")).toBe(true);
+  expect(document.querySelector(".modal-dialog")).toBeTruthy();
 });
 
 test("passes on contentRef", () => {
@@ -16,7 +14,7 @@ test("passes on contentRef", () => {
     contentRef.current = element;
   };
 
-  mount(<ModalDialog contentRef={contentRef} />);
+  render(<ModalDialog contentRef={contentRef} />);
 
   expect(contentRef.current).not.toBe(undefined);
 });
