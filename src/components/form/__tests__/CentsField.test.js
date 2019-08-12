8441d2cda0255307416d309e8405da5695f18780
Kevin Deisz <kevin.deisz@gmail.com>
2019-08-12 13:40:55 -0400

    Switch CentsField over to RTL

--- src/components/form/__tests__/CentsField.test.js
+++ src/components/form/__tests__/CentsField.test.js
@@ -1,40 +1,47 @@
 import React from "react";
-import { mount } from "enzyme";
+import { fireEvent, render } from "@testing-library/react";
 
 import CentsField from "../CentsField";
 
+test("has no violations", () => (
+  expect(<CentsField name="cents">Cents</CentsField>).toHaveNoViolations()
+));
+
 test("calls up to callbacks if they are provided", () => {
   const onChange = jest.fn();
-  const component = mount(<CentsField name="cents" onChange={onChange} />);
+  const { getByRole } = render(<CentsField name="cents" onChange={onChange} />);
 
-  component.find("input").simulate("change", { target: { value: 1.23 } });
+  fireEvent.change(getByRole("textbox"), { target: { value: 1.23 } });
 
   expect(onChange).toHaveBeenCalledWith(123);
 });
 
 test("displays the value using cents", () => {
-  const component = mount(<CentsField name="cents" value={123} />);
+  const { getByRole } = render(<CentsField name="cents" value={123} />);
 
-  expect(component.find("input").props().value).toEqual(1.23);
+  expect(getByRole("textbox")).toHaveProperty("value", "1.23");
 });
 
 test("validates that the value cannot be <= 0", () => {
   const onError = jest.fn();
-  mount(<CentsField name="cents" value={-5} onError={onError} />);
+  render(<CentsField name="cents" value={-5} onError={onError} />);
 
   expect(onError).toHaveBeenCalled();
 });
 
 test("handles cases where the value is empty", () => {
   const onChange = jest.fn();
-  const component = mount(<CentsField name="cents" onChange={onChange} />);
+  const { getByRole } = render(
+    <CentsField name="cents" onChange={onChange} value={123} />
+  );
 
-  component.find("input").simulate("change", { target: { value: "" } });
+  fireEvent.change(getByRole("textbox"), { target: { value: "" } });
 
   expect(onChange).toHaveBeenLastCalledWith(null);
 });
 
 test("functions without an onChange", () => {
-  const component = mount(<CentsField name="cents" />);
-  component.find("input").simulate("change", { target: { value: "" } });
+  const { getByRole } = render(<CentsField name="cents" />);
+
+  fireEvent.change(getByRole("textbox"), { target: { value: "" } });
 });
