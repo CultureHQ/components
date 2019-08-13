386db70a156a02d819f419d0132a229881570adf
Kevin Deisz <kevin.deisz@gmail.com>
2019-08-13 13:34:47 -0400

    Switch Nav over to RTL

--- src/components/__tests__/Nav.test.js
+++ src/components/__tests__/Nav.test.js
@@ -1,50 +1,45 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { fireEvent, render } from "@testing-library/react";
 
 import Nav from "../Nav";
 
-test("has no violations", async () => {
-  await expect(<Nav>This is a nav.</Nav>).toHaveNoViolations();
-});
+test("has no violations", () => (
+  expect(<Nav>This is a nav.</Nav>).toHaveNoViolations()
+));
 
 test("renders without crashing", () => {
   const message = "This is a nav.";
-  const component = shallow(<Nav>{message}</Nav>);
+  const { queryByText } = render(<Nav>{message}</Nav>);
 
-  expect(component.html()).toContain(message);
+  expect(queryByText(message)).toBeTruthy();
 });
 
 test("passes on className", () => {
-  const component = shallow(<Nav className="nav" />);
+  const { container } = render(<Nav className="nav" />);
 
-  expect(component.hasClass("nav")).toBe(true);
-  expect(component.hasClass("chq-nav")).toBe(true);
+  expect(container.firstChild.classList).toContain("nav");
 });
 
 test("hides the nav when the page is scrolled down", () => {
   window.pageYOffset = 25;
 
-  const component = shallow(<Nav />);
-  expect(component.hasClass("chq-nav-hd")).toBe(false);
+  const { container } = render(<Nav />);
+  expect(container.firstChild.classList).not.toContain("chq-nav-hd");
 
   window.pageYOffset = 50;
-  component.instance().handleWindowScroll();
-  component.update();
+  fireEvent.scroll(window);
 
-  expect(component.hasClass("chq-nav-hd")).toBe(true);
-  component.unmount();
+  expect(container.firstChild.classList).toContain("chq-nav-hd");
 });
 
 test("shows the nav when the page is scrolled up", () => {
   window.pageYOffset = 100;
 
-  const component = shallow(<Nav />);
-  expect(component.hasClass("chq-nav-hd")).toBe(false);
+  const { container } = render(<Nav />);
+  expect(container.firstChild.classList).not.toContain("chq-nav-hd");
 
   window.pageYOffset = 50;
-  component.instance().handleWindowScroll();
-  component.update();
+  fireEvent.scroll(window);
 
-  expect(component.hasClass("chq-nav-hd")).toBe(false);
-  component.unmount();
+  expect(container.firstChild.classList).not.toContain("chq-nav-hd");
 });
