8fb2c1b996ef543bb9c66f4d399da3fa90d686fd
Kevin Deisz <kevin.deisz@gmail.com>
2019-08-13 16:19:29 -0400

    Switch Tooltip over to RTL

--- src/components/__tests__/Tooltip.test.js
+++ src/components/__tests__/Tooltip.test.js
@@ -1,19 +1,22 @@
 import React from "react";
-import { mount } from "enzyme";
+import { render } from "@testing-library/react";
 
 import Tooltip from "../Tooltip";
 
-test("has no violations", async () => {
-  const component = <Tooltip tip="Tip">Inner content</Tooltip>;
-
-  await expect(component).toHaveNoViolations();
+beforeEach(() => {
+  window.requestAnimationFrame = jest.fn();
 });
 
-test("resizes when the window resizes and dedups resize events", done => {
-  const component = mount(<Tooltip tip="Tip">Inner content</Tooltip>);
+afterEach(() => {
+  window.requestAnimationFrame.mockRestore();
+});
 
-  const recompute = jest.fn();
-  component.instance().requestComputeOffsets = recompute;
+test("has no violations", () => (
+  expect(<Tooltip tip="Tip">Inner content</Tooltip>).toHaveNoViolations()
+));
+
+test("resizes when the window resizes and dedups resize events", done => {
+  const { unmount } = render(<Tooltip tip="Tip" />);
 
   global.innerWidth = 200;
   global.dispatchEvent(new Event("resize"));
@@ -21,38 +24,19 @@ test("resizes when the window resizes and dedups resize events", done => {
   global.dispatchEvent(new Event("resize"));
 
   setTimeout(() => {
-    expect(recompute).toHaveBeenCalledTimes(1);
-    component.unmount();
+    expect(window.requestAnimationFrame).toHaveBeenCalledTimes(2);
+    unmount();
     done();
   }, 1000);
 });
 
-test("ensures the bubble is not too far to the left", () => {
-  const component = mount(<Tooltip tip="Tip">Inner content</Tooltip>);
-  const style = {};
-
-  component.instance().bubble.current = { offsetWidth: 300, style };
-  component.instance().computeOffsets();
-
-  expect(style.left).toEqual("10px");
-});
-
-test("does not break when one of the refs is null", () => {
-  const component = mount(<Tooltip tip="Tip">Inner content</Tooltip>);
-  component.instance().bubble.current = null;
-
-  expect(() => component.instance().computeOffsets()).not.toThrow();
-});
-
 test("recomputes offsets when the tip changes", () => {
-  const component = mount(<Tooltip tip="Tip">Inner content</Tooltip>);
+  const { rerender } = render(<Tooltip tip="Tip" />);
+  expect(window.requestAnimationFrame).toHaveBeenCalledTimes(1);
 
-  const requestComputeOffsets = jest.fn();
-  component.instance().requestComputeOffsets = requestComputeOffsets;
+  rerender(<Tooltip tip="Tip" />);
+  expect(window.requestAnimationFrame).toHaveBeenCalledTimes(1);
 
-  component.setProps({ tip: "Tip" });
-  expect(requestComputeOffsets).not.toHaveBeenCalled();
-
-  component.setProps({ tip: "A different tip" });
-  expect(requestComputeOffsets).toHaveBeenCalledTimes(1);
+  rerender(<Tooltip tip="A different tip" />);
+  expect(window.requestAnimationFrame).toHaveBeenCalledTimes(2);
 });
