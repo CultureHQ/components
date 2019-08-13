dfb7b3414b49ee2cc30d15de92391ad2bef48233
Kevin Deisz <kevin.deisz@gmail.com>
2019-08-13 13:16:07 -0400

    Switch DateTimeField over to RTL

--- src/components/form/__tests__/DateTimeField.test.js
+++ src/components/form/__tests__/DateTimeField.test.js
@@ -1,50 +1,59 @@
 import React from "react";
-import { mount } from "enzyme";
+import { fireEvent, render } from "@testing-library/react";
 
 import DateTimeField from "../DateTimeField";
 import Form from "../Form";
+import SubmitButton from "../SubmitButton";
 
 test("functions as expected", () => {
   const onSubmit = jest.fn(() => Promise.resolve());
-  const initialValues = { datetime: new Date(2018, 0, 1, 0, 0, 0).toISOString() };
+  const initialValues = {
+    datetime: new Date(2018, 0, 1, 0, 0, 0).toISOString()
+  };
 
-  const component = mount(
+  const { getByLabelText, getByText } = render(
     <Form onSubmit={onSubmit} initialValues={initialValues}>
       <DateTimeField name="datetime" />
+      <SubmitButton />
     </Form>
   );
 
-  component.find("button.chq-ffd--ctrl").simulate("click");
-  component.find("button.chq-cal--day").at(15).simulate("click");
-  component.find("button.chq-tsl--op").at(52).simulate("click");
-  component.instance().submit();
+  fireEvent.click(getByLabelText("Open dialog"));
+  fireEvent.click(getByText("15"));
+  fireEvent.click(getByText("01:00 PM"));
+  fireEvent.click(getByText("Submit"));
 
-  expect(onSubmit.mock.calls[0][0].datetime).toEqual("2018-01-15T13:00:00.000Z");
+  const { datetime } = onSubmit.mock.calls[0][0];
+  expect(datetime).toEqual("2018-01-15T13:00:00.000Z");
 });
 
 test("works without a value passed in", () => {
-  const component = mount(<DateTimeField />);
+  const { queryAllByRole } = render(<DateTimeField />);
 
-  expect(component.find("button")).toHaveLength(1);
+  expect(queryAllByRole("button")).toHaveLength(1);
 });
 
 test("allows clicking on select before making a time selection", () => {
   const onChange = jest.fn();
-  const component = mount(<DateTimeField onChange={onChange} />);
+  const { getByLabelText, getByText } = render(
+    <DateTimeField onChange={onChange} />
+  );
 
-  component.find("button.chq-ffd--ctrl").simulate("click");
-  component.find("button.chq-cal--day").at(15).simulate("click");
-  component.find("Button").simulate("click");
+  fireEvent.click(getByLabelText("Open dialog"));
+  fireEvent.click(getByText("15"));
+  fireEvent.click(getByText("Select"));
 
   expect(onChange.mock.calls[1][0].endsWith("T12:00:00.000Z")).toBe(true);
 });
 
 test("works when clicking on a time before a date", () => {
   const onChange = jest.fn();
-  const component = mount(<DateTimeField onChange={onChange} />);
+  const { getByLabelText, getByText } = render(
+    <DateTimeField onChange={onChange} />
+  );
 
-  component.find("button.chq-ffd--ctrl").simulate("click");
-  component.find("button.chq-tsl--op").at(52).simulate("click");
+  fireEvent.click(getByLabelText("Open dialog"));
+  fireEvent.click(getByText("01:00 PM"));
 
   expect(onChange.mock.calls[0][0].endsWith("T13:00:00.000Z")).toBe(true);
 });
