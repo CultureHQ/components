36d4609bc62e0dd19af63ed7ed7cbeed4c8f607e
Kevin Deisz <kevin.deisz@gmail.com>
2019-09-05 11:14:50 -0400

    Convert over FeedItem

---
+++ src/components/FeedItem.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+
+import classnames from "../classnames";
+import { HTMLContainerProps } from "../types";
+
+const FeedItem = ({ children, className }: HTMLContainerProps) => (
+  <div className={classnames("chq-fdi", className)}>
+    {children}
+  </div>
+);
+
+const FeedItemBody = ({ children, className }: HTMLContainerProps) => (
+  <div className={classnames("chq-fdi--bd", className)}>
+    {children}
+  </div>
+);
+
+const FeedItemFooter = ({ children, className }: HTMLContainerProps) => (
+  <div className={classnames("chq-fdi--ft", className)}>
+    {children}
+  </div>
+);
+
+FeedItem.Body = FeedItemBody;
+FeedItem.Footer = FeedItemFooter;
+
+export default FeedItem;
