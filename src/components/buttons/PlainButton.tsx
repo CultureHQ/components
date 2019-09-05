9af3e742ea6fce1a8f6746c5b17f7f0d696dac69
Kevin Deisz <kevin.deisz@gmail.com>
2019-09-05 11:28:03 -0400

    Convert over PlainButton props

---
+++ src/components/buttons/PlainButton.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+import classnames from "../../classnames";
+import { HTMLContainerProps } from "../../types";
+
+type PlainButtonProps = HTMLContainerProps & React.HTMLProps<HTMLButtonElement>;
+
+const PlainButton = React.forwardRef<HTMLButtonElement, PlainButtonProps>(
+  ({ children, className, ...props }, ref) => (
+    <button {...props} ref={ref} type="button" className={classnames("chq-pbn", className)}>
+      {children}
+    </button>
+  )
+);
+
+PlainButton.displayName = "PlainButton";
+
+export default PlainButton;
