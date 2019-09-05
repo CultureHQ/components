7bb873423a4e99011a05ad31f7bd9723b4c8dd81
Kevin Deisz <kevin.deisz@gmail.com>
2019-09-05 10:42:09 -0400

    Convert over Success and Info

--- src/components/Success.tsx
+++ src/components/Success.tsx
@@ -2,15 +2,15 @@ import * as React from "react";
 
 import classnames from "../classnames";
 
-type WarningProps = {
+type SuccessProps = {
   children: React.ReactNode;
   className?: string;
 };
 
-const Warning = ({ children, className }: WarningProps) => (
-  <div className={classnames("chq-wrn", className)}>
+const Success = ({ children, className }: SuccessProps) => (
+  <div className={classnames("chq-scs", className)}>
     <p>{children}</p>
   </div>
 );
 
-export default Warning;
+export default Success;
