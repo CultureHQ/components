9928786c740fe925465876a156072525c682993a
Kevin Deisz <kevin.deisz@gmail.com>
2019-09-05 11:10:35 -0400

    Convert over Spinner element

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import * as React from "react";
 
 import classnames from "../classnames";
 import Circles from "./Circles";
 
-const Spinner = ({ className, ...props }) => (
+type SpinnerProps = React.HTMLProps<HTMLDivElement> & {
+  className?: string;
+};
+
+const Spinner = ({ className, ...props }: SpinnerProps) => (
   <div className={classnames("chq-spn", className)} {...props}>
     <Circles />
   </div>
