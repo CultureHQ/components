aacc5457f910b622ccc291e8afa75c5154becd94
Kevin Deisz <kevin.deisz@gmail.com>
2019-09-05 10:44:28 -0400

    Convert over Table

--- src/components/Table.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import classnames from "../classnames";
-
-const Table = ({ className, children }) => (
-  <table className={classnames("chq-tbl", className)}>{children}</table>
-);
-
-export default Table;
