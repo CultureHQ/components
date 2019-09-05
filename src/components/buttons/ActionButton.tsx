2d60ca88bbc2e4a22eebfecfcf6a936ba23e179e
Kevin Deisz <kevin.deisz@gmail.com>
2019-09-05 11:17:51 -0400

    Convert over ActionButton

---
+++ src/components/buttons/ActionButton.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+import classnames from "../../classnames";
+import { HTMLContainerProps } from "../../types";
+
+import Icon from "../Icon";
+
+type ActionButtonProps = HTMLContainerProps & React.HTMLProps<HTMLButtonElement> & {
+  icon?: string;
+};
+
+const ActionButton = ({ children, className, icon, ...props }: ActionButtonProps) => (
+  <button {...props} type="button" className={classnames("chq-abn", className)}>
+    {icon && <><Icon icon={icon} />{" "}</>}
+    {children}
+  </button>
+);
+
+export default ActionButton;
