cdbd0223ad9c765096d1af0c0d2b29290d57df47
Kevin Deisz <kevin.deisz@gmail.com>
2019-09-05 10:43:23 -0400

    Convert over Tag component

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import * as React from "react";
 
 import classnames from "../classnames";
 
-const Tag = ({ children, className, color = "blue" }) => (
+type TagProps = {
+  children: React.ReactNode;
+  className?: string;
+  color: "blue" | "gray" | "red";
+};
+
+const Tag = ({ children, className, color = "blue" }: TagProps) => (
   <div
     className={
       classnames("chq-tag", className, {
