281142c6f7a7a9926336b42ce63c34b8b8a42820
Kevin Deisz <kevin.deisz@gmail.com>
2019-09-06 15:45:46 -0400

    Type the ImagePreview component

---
+++ src/components/ImagePreview.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+
+import readImage from "../utils/readImage";
+
+type ImagePreviewProps = {
+  image: Blob | File | null;
+  preview: Blob | File | string | null;
+};
+
+type ImagePreviewState = {
+  src: string | null;
+  styles: { [key: string]: undefined | string | number };
+};
+
+class ImagePreview extends React.Component<ImagePreviewProps, ImagePreviewState> {
+  private componentIsMounted: boolean = false;
+
+  private containerRef = React.createRef<HTMLSpanElement>();
+
+  state = { src: null, styles: {} };
+
+  componentDidMount() {
+    this.componentIsMounted = true;
+
+    const { preview } = this.props;
+
+    if (preview) {
+      this.enqueueLoad();
+    }
+  }
+
+  componentDidUpdate(prevProps: ImagePreviewProps) {
+    const { preview } = this.props;
+
+    if (preview && (preview !== prevProps.preview)) {
+      this.enqueueLoad();
+    }
+  }
+
+  componentWillUnmount() {
+    this.componentIsMounted = false;
+  }
+
+  enqueueLoad() {
+    const { image, preview } = this.props;
+    const container = this.containerRef.current;
+
+    if (container && container.parentNode instanceof HTMLElement) {
+      const { clientWidth, clientHeight } = container.parentNode;
+
+      readImage(image, preview, clientWidth, clientHeight).then(({ src, styles }) => {
+        if (this.componentIsMounted) {
+          this.setState({ src, styles });
+        }
+      });
+    }
+  }
+
+  render() {
+    const { src, styles } = this.state;
+
+    return (
+      <span ref={this.containerRef}>
+        {src && <img className="chq-ipv" src={src || undefined} alt="Preview" style={styles} />}
+      </span>
+    );
+  }
+}
+
+export default ImagePreview;
