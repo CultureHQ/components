f1d8f5b63bec34ddbc31198a892bcedb38d7b028
Kevin Deisz <kevin.deisz@gmail.com>
2019-09-10 12:28:13 -0400

    Simplify with @culturehq/hooks

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,44 +1,29 @@
 import * as React from "react";
+import { useWindowEvent } from "@culturehq/hooks";
 
 import classnames from "../classnames";
 import { HTMLContainerProps } from "../typings";
 
-const makeInitialState = () => ({
-  displayed: true,
-  scroll: window.pageYOffset
-});
+const makeUpdate = current => {
+  const scroll = window.pageYOffset;
 
-type NavState = { displayed: boolean, scroll: number };
-type NavAction = { type: "scroll", scroll: number };
-
-const reducer = (state: NavState, action: NavAction) => {
-  switch (action.type) {
-    case "scroll":
-      return {
-        ...state,
-        displayed: state.scroll === 0 || action.scroll <= 30 || state.scroll > action.scroll,
-        scroll: action.scroll
-      };
-    default:
-      throw new Error();
-  }
+  return {
+    displayed: current.scroll === 0 || scroll <= 30 || current.scroll > scroll,
+    scroll
+  };
 };
 
-type NavProps = HTMLContainerProps & React.HTMLProps<HTMLElement>;
+type NavProps = HTMLContainerProps & React.HTMLAttributes<HTMLElement>;
 
 const Nav = ({ children, className, ...props }: NavProps) => {
-  const [state, dispatch] = React.useReducer<typeof reducer, null>(reducer, null, makeInitialState);
+  const [state, setState] = React.useState(() => ({
+    displayed: true,
+    scroll: window.pageYOffset
+  }));
 
-  React.useEffect(
-    () => {
-      const onScroll = () => {
-        dispatch({ type: "scroll", scroll: window.pageYOffset });
-      };
-
-      window.addEventListener("scroll", onScroll);
-      return () => window.removeEventListener("scroll", onScroll);
-    },
-    [dispatch]
+  useWindowEvent(
+    "scroll",
+    React.useCallback(() => setState(makeUpdate), [setState])
   );
 
   return (
