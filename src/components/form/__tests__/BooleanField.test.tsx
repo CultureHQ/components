82ca68b7cb8c8fd332307a4eabc9256be137460a
Kevin Deisz <kevin.deisz@gmail.com>
2019-09-11 07:51:10 -0400

    Convert over BooleanField test

--- src/components/form/__tests__/BooleanField.test.tsx
+++ src/components/form/__tests__/BooleanField.test.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import * as React from "react";
 import { fireEvent, render } from "@testing-library/react";
 
 import BooleanField from "../BooleanField";
 
 test("calls up to callbacks if they are provided", () => {
   const onChange = jest.fn();
-  const { getByRole } = render(<BooleanField name="boolean" onChange={onChange} />);
+  const { getByRole } = render(
+    <BooleanField name="boolean" onChange={onChange}>
+      Boolean!
+    </BooleanField>
+  );
 
   fireEvent.click(getByRole("button"));
 
@@ -13,7 +17,11 @@ test("calls up to callbacks if they are provided", () => {
 });
 
 test("works with initial values", () => {
-  const { getByRole } = render(<BooleanField name="boolean" value />);
+  const { getByRole } = render(
+    <BooleanField name="boolean" value>
+      Boolean!
+    </BooleanField>
+  );
 
   expect(getByRole("button").classList).toContain("chq-cmk-ck");
 
