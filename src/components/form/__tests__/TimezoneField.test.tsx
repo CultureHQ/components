6ed434bf4d137b7162fad8c31a3699cb0b05577b
Kevin Deisz <kevin.deisz@gmail.com>
2019-09-11 08:00:03 -0400

    Switch over TimezoneField test

--- src/components/form/__tests__/TimezoneField.test.tsx
+++ src/components/form/__tests__/TimezoneField.test.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import * as React from "react";
 import { render, waitForDomChange } from "@testing-library/react";
 
 import TimezoneField from "../TimezoneField";
 
 test("waits for timezones before switching to a select", async () => {
   const { container, getByLabelText, getByRole } = render(
-    <TimezoneField name="timezone" />
+    <TimezoneField name="timezone">
+      Timezones!
+    </TimezoneField>
   );
 
   expect(getByRole("textbox")).toBeTruthy();
