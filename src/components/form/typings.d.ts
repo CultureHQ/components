5c934920ddded0bb2e6309a91a080569569619c2
Kevin Deisz <kevin.deisz@gmail.com>
2019-09-13 10:16:09 -0400

    Consolidate passed props typings

--- src/components/form/typings.d.ts
+++ src/components/form/typings.d.ts
@@ -1,7 +1,26 @@
 export type FormFieldError = string | null;
 
 export type SelectValue = string;
+
 export type SelectOption = {
   label: string;
   value: SelectValue;
+};
+
+export type SelectFieldPassedProps = {
+  creatable: boolean;
+  display: string;
+  filteredOptions: SelectOption[];
+  inputRef: React.RefObject<HTMLInputElement>;
+  multiple: boolean;
+  name: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onClose: () => void;
+  onDeselect: (value: SelectValue) => void;
+  onOpen: () => void;
+  onSelect: (value: SelectValue) => void;
+  open: boolean;
+  options: SelectOption[];
+  placeholder: string;
+  value: SelectValue | SelectValue[];
 };
