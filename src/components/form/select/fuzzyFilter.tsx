95fe9b40c37f7c21bf6f0e71ed547630795d7432
Kevin Deisz <kevin.deisz@gmail.com>
2019-09-13 15:10:40 -0400

    Type out fuzzyFilter

---
+++ src/components/form/select/fuzzyFilter.tsx
@@ -0,0 +1,21 @@
+import { SelectOption } from "../typings";
+
+const makeSegments = (value: string) => (
+  value.toLowerCase().split(" ").filter(Boolean)
+);
+
+const fuzzyFilter = (options: SelectOption[], matchable: string) => {
+  if (!matchable) {
+    return options;
+  }
+
+  const terms = makeSegments(matchable);
+
+  return options.filter(
+    ({ label }) => makeSegments(label).some(segment => (
+      terms.some(term => segment.startsWith(term))
+    ))
+  );
+};
+
+export default fuzzyFilter;
