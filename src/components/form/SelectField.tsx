ddd798a2381a5bed962a91ead71401864cb41d70
Kevin Deisz <kevin.deisz@gmail.com>
2019-09-13 15:08:19 -0400

    Type of SelectField

--- src/components/form/SelectField.tsx
+++ src/components/form/SelectField.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import * as React from "react";
 
 import classnames from "../../classnames";
 import SelectFieldSingle from "./select/SelectFieldSingle";
 import SelectFieldMulti from "./select/SelectFieldMulti";
 import { withForm } from "./Form";
 
-const SelectField = ({ children = null, className = null, multiple = false, ...props }) => (
+type SelectFieldCommonProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+type SelectFieldProps = SelectFieldCommonProps & (
+  ({ multiple?: false } & React.ComponentProps<typeof SelectFieldSingle>)
+  | ({ multiple: true } & React.ComponentProps<typeof SelectFieldMulti>)
+);
+
+const SelectField = ({ children, className, multiple, ...props }: SelectFieldProps) => (
   <label className={classnames("chq-ffd", className)} htmlFor={props.name}>
     <span className="chq-ffd--lb">{children}</span>
     {multiple ? <SelectFieldMulti {...props} /> : <SelectFieldSingle {...props} />}
