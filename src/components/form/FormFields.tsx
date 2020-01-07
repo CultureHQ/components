c348b3588fb02068e4e337ca085cd4947d8c11b1
Kevin Deisz <kevin.deisz@gmail.com>
2020-01-07 11:07:39 -0500

    Convert FormFields to use hooks

--- src/components/form/FormFields.tsx
+++ src/components/form/FormFields.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import classnames from "../../classnames";
 import FormError from "./FormError";
-import { FormState, withForm } from "./Form";
+import { useForm } from "./Form";
+import useAutoFocus from "./select/useAutoFocus";
 import { FormFieldError } from "./typings";
 
 type HijackedProps = "className" | "name" | "onChange" | "required" | "value";
@@ -22,79 +23,61 @@ type FormFieldState = {
   touched: boolean;
 };
 
-const buildFormField = (type: string, displayName: string) => withForm(
-  class extends React.Component<FormFieldProps & FormState, FormFieldState> {
-    private inputRef = React.createRef<HTMLInputElement>();
+const makeFormField = (type: string) => {
+  const FormField: React.FC<FormFieldProps> = ({
+    addon, autoFocus, children, className, name, onChange, required, validator, value, ...props
+  }) => {
+    const { submitted, values, onError, onFormChange } = useForm();
 
-    static displayName = displayName;
+    const inputRef = useRef<HTMLInputElement | null>(null);
+    const [touched, setTouched] = useState<boolean>(false);
 
-    state = { touched: false };
+    useAutoFocus(autoFocus || false, inputRef);
 
-    componentDidMount() {
-      const { autoFocus } = this.props;
-      const input = this.inputRef.current;
-
-      if (autoFocus && input) {
-        input.focus();
-      }
-    }
-
-    handleBlur = () => {
-      this.setState({ touched: true });
-    };
-
-    handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const { name, onChange, onFormChange } = this.props;
-      const { value } = event.target;
+    const onBlur = () => setTouched(true);
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { value: nextValue } = event.target;
 
       if (onChange) {
-        onChange(value);
+        onChange(nextValue);
       }
-      onFormChange(name, value);
+      onFormChange(name, nextValue);
     };
 
-    render() {
-      const {
-        addon, autoFocus, children, className, errors, name, onError,
-        onFormChange, required, submitted, submitting, validator, value, values,
-        ...props
-      } = this.props;
+    const normal = value || (values[name] as undefined | string);
 
-      const { touched } = this.state;
+    return (
+      <label className={classnames("chq-ffd", className)} htmlFor={name}>
+        <span className="chq-ffd--lb">{children}</span>
+        {addon && <span className="chq-ffd--ad">{addon}</span>}
+        <input
+          className="chq-ffd--ctrl"
+          ref={inputRef}
+          {...props}
+          type={type}
+          id={name}
+          name={name}
+          value={normal || ""}
+          onBlur={onBlur}
+          onChange={handleChange}
+        />
+        <FormError
+          name={name}
+          onError={onError}
+          required={required}
+          submitted={submitted}
+          touched={touched}
+          validator={validator}
+          value={normal}
+        />
+      </label>
+    );
+  };
 
-      const normal = value || (values[name] as undefined | string);
+  return FormField;
+};
 
-      return (
-        <label className={classnames("chq-ffd", className)} htmlFor={name}>
-          <span className="chq-ffd--lb">{children}</span>
-          {addon && <span className="chq-ffd--ad">{addon}</span>}
-          <input
-            className="chq-ffd--ctrl"
-            ref={this.inputRef}
-            {...props}
-            type={type}
-            id={name}
-            name={name}
-            value={normal || ""}
-            onBlur={this.handleBlur}
-            onChange={this.handleChange}
-          />
-          <FormError
-            name={name}
-            onError={onError}
-            required={required}
-            submitted={submitted}
-            touched={touched}
-            validator={validator}
-            value={normal}
-          />
-        </label>
-      );
-    }
-  }
-);
-
-export const EmailField = buildFormField("email", "EmailField");
-export const NumberField = buildFormField("number", "NumberField");
-export const PasswordField = buildFormField("password", "PasswordField");
-export const StringField = buildFormField("text", "StringField");
+export const EmailField = makeFormField("email");
+export const NumberField = makeFormField("number");
+export const PasswordField = makeFormField("password");
+export const StringField = makeFormField("text");
