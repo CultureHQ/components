38edf2a5ff181147ad59b481d38b6ee20971b945
Kevin Deisz <kevin.deisz@gmail.com>
2020-01-08 11:21:14 -0500

    Fix up compile

--- src/components/form/__tests__/TimezoneField.test.tsx
+++ src/components/form/__tests__/TimezoneField.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, waitForDomChange } from "@testing-library/react";
 
-import TimezoneField from "../TimezoneField";
+import TimezoneField, { Timezone } from "../TimezoneField";
 import timezones from "../../../timezones.json";
 
 test("waits for timezones before switching to a select", async () => {
@@ -28,7 +28,10 @@ test("fires onOffsetChange when the component has fetched the timezones", async 
 
   await waitForDomChange({ container });
 
-  const { label, offset } = timezones.find(timezone => timezone.value === "America/New_York");
-  expect(getByLabelText("Value").value).toEqual(label);
+  const match = timezones.find(timezone => timezone.value === "America/New_York");
+  expect(match).not.toBe(null);
+
+  const { label, offset } = match as Timezone;
+  expect((getByLabelText("Value") as HTMLInputElement).value).toEqual(label);
   expect(onOffsetChange).toHaveBeenCalledWith(offset);
 });
